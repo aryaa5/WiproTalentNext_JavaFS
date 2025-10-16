b257f8ee870f9eb45cfa7a173497ca23edf7bc3f
aryaa5 <128314184+aryaa5@users.noreply.github.com>
2025-10-16 11:42:48 +0530

    Create NullStudentException.java

---
+++ J Unit/mini project/com/mile1/exception/NullStudentException.java
@@ -0,0 +1,10 @@
+package com.mile1.exception;
+
+public class NullStudentException extends Exception {
+    public NullStudentException() {
+        super();
+    }
+    public NullStudentException(String message) {
+        super(message);
+    }
+}
