1f8b01fbdf0d581665953d942dd082ff5cffb098
aryaa5 <128314184+aryaa5@users.noreply.github.com>
2025-10-16 11:43:51 +0530

    Create NullMarksArrayException.java

---
+++ J Unit/mini project/com/mile1/exception/NullMarksArrayException.java
@@ -0,0 +1,10 @@
+package com.mile1.exception;
+
+public class NullMarksArrayException extends Exception {
+    public NullMarksArrayException() {
+        super();
+    }
+    public NullMarksArrayException(String message) {
+        super(message);
+    }
+}
