cc04b604fc456244ff1600528491ee74462d91c9
aryaa5 <128314184+aryaa5@users.noreply.github.com>
2025-10-16 21:37:58 +0530

    Create CashbackSystemApplication.java

---
+++ Spring/mini projects/Cash Back Offer/src/main/java/com/legionbank/cashback/CashbackSystemApplication.java
@@ -0,0 +1,11 @@
+package com.legionbank.cashback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbackSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CashbackSystemApplication.class, args);
+    }
+}
