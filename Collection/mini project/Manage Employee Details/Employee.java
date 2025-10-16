ccbdcddd998632f9eb40be7496dc37955416dde8
aryaa5 <128314184+aryaa5@users.noreply.github.com>
2025-10-16 11:00:50 +0530

    Create Employee.java

---
+++ Collection/mini project/Manage Employee Details/Employee.java
@@ -0,0 +1,30 @@
+public class Employee implements Comparable<Employee> {
+    private String firstName;
+    private String lastName;
+    private String mobile;
+    private String email;
+    private String address;
+
+    public Employee(String firstName, String lastName, String mobile, String email, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mobile = mobile;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public int compareTo(Employee other) {
+        return this.firstName.compareToIgnoreCase(other.firstName);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s %-15s %-15s %-30s %-15s",
+                firstName, lastName, mobile, email, address);
+    }
+}
